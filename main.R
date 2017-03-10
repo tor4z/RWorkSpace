22217dcfab519ab188a928d28ed23bde71429d01
tor4z <vwenjie@hotmail.com>
2017-03-10 22:47:43 +0800

    Add ts sim

--- main.R
+++ main.R
@@ -20,3 +20,21 @@ y <- sim(result$net, P)
 plot(P,y, col="blue", pch="+")
 
 points(P,target, col="red", pch="x")
+
+
+## TS_BK
+ts.sim <- function(p, q, tb = 20, xe = 100) {
+    x <- seq(1, xe, by = 0.1)
+    y <- p*(1-q^(x - tb))/(1-q)
+    y[1:tb] <- 0
+    hx = log10(0.5)/log10(q)
+    
+    df <- data.frame(x = x, y = y)
+    qplot(x, y, data = df, geom = "line") +
+        geom_vline(aes(xintercept  = hx), colour = "red") +
+        geom_text(aes(x = hx, y = 5, label = round(hx, digits = 2))) + 
+        geom_text(aes(x = xe, y = max(y), label = round(max(y), digits = 2)))
+}
+
+ts.sim(1, 0.9)
+
