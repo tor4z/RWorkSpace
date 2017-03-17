6a031d8ba97972f221dcdb828088ac5617b41ba9
tor4z <vwenjie@hotmail.com>
2017-03-18 00:19:50 +0800

    Add packages manager script

---
+++ packageManage.R
@@ -0,0 +1,13 @@
+packagesFile = "./data/packages.RData"
+
+packages.save <- function() {
+    packages <- as.data.frame(installed.packages())
+    save(packages, file = packagesFile)
+}
+
+packages.restore <- function() {
+    load(file = packagesFile)
+    install.packages(as.character(packages$Package))
+}
+
+packages.save()
